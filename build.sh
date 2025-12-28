6915ea51a6bbdd6ff1f46e5a3779dd491c31c9d1
Ezeagu Kosi <kosialex66@gmail.com>
2025-12-28 04:09:15 +0100

    Add Django backend files

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python backend/main/manage.py collectstatic --no-input
+python main/manage.py collectstatic --no-input
 
-python backend/main/manage.py migrate
+python main/manage.py migrate
